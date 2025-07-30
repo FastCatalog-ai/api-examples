3e1951f577a9159f798496798a78224f04a9b876
Nicolas Schifano <6748472+nicoschif@users.noreply.github.com>
2025-07-30 12:51:39 -0400

    update

--- examples/2_find_and_retrieve_details/find_and_retrieve_details.py
+++ examples/2_find_and_retrieve_details/find_and_retrieve_details.py
@@ -35,7 +35,7 @@ def make_summary_dict(data: dict) -> dict:
 BASE_URL = "https://api.fastcatalog.ai/v1"
 
 SEARCH_API_URL = f"{BASE_URL}/catalog-entries"
-TOKEN = os.getenv("FASTCATALOG_TOKEN", "H1ybgsyxQ8H1xZCrGJS0sWWZy2GACmZUn8w8hlu0QI")   # Replace with your token
+TOKEN = os.getenv("FASTCATALOG_TOKEN", "YOUR_BEARER_TOKEN")   # Replace with your token
 
 payload = {
     "homepages": ["https://huggingface.co/datasets/Salesforce/xlam-function-calling-60k"]
